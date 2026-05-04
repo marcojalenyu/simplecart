23549e35265d331fb6352181f996ab1350852834
marcojalenyu <112309140+marcojalenyu@users.noreply.github.com>
2026-05-04 16:14:19 +0800

    feat: Add to cart logic

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,10 +1,25 @@
 import { Product } from "@/domain/product/product.types";
+import { useAuth } from "@/hooks/useAuth";
+import { userService } from "@/services/user.service";
 import { Pressable } from "react-native";
 import { ThemedText } from "../themed-text";
 import { ThemedView } from "../themed-view";
 import { Card, ImageCard } from "../ui/Card";
 
 export function ProductCard({ product }: { product: Product }) {
+  const { user, setUser } = useAuth();
+
+  const handleAddToCart = async () => {
+    if (!user) return;
+    const updatedUser = await userService.addToCart(
+      user.username,
+      product.id,
+      1,
+    );
+    setUser(updatedUser);
+    alert(`${product.productName} added to cart!`);
+  };
+
   return (
     <Card className="gap-1 w-[48%]">
       <ImageCard
@@ -16,11 +31,7 @@ export function ProductCard({ product }: { product: Product }) {
           <ThemedText type="defaultSemiBold">{product.productName}</ThemedText>
           <ThemedText type="default">₱{product.price.toFixed(2)}</ThemedText>
         </ThemedView>
-        <Pressable
-          onPress={() => {
-            console.log("Add to cart:", product.id);
-          }}
-        >
+        <Pressable onPress={handleAddToCart}>
           <ThemedText type="title">+</ThemedText>
         </Pressable>
       </ThemedView>
